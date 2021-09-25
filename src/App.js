004d75727ee4897394c296ca7e9096bcc9611e7b
akturanilyas <akturanilyas@gmail.com>
2021-09-25 23:38:34 +0300

    Delete: Remove router architecture and designed simplyied

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import ROUTES, {RenderRoutes} from "./routes";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import Login from "./components/auth/login";
+import Home from "./components/home/home";
+import SignUp from "./components/auth/signup";
 
 
 function App() {
     return (
-        <RenderRoutes routes={ROUTES} />
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/" exact>
+                        <Login/>
+                    </Route>
+                    <Route path="/auth/login" exact>
+                        <Login/>
+                    </Route>
+                    <Route path="/auth/sign-up" exact>
+                        <SignUp/>
+                    </Route>
+                    <Route path="/home" exact >
+                        <Home/>
+                    </Route>
+                </Switch>
+            </div>
+
+        </Router>
     );
 }
 
